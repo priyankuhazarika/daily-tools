d465247c45bef8c239218b931be0a89bf09e828f
Priyanku Hazarika <priyankuhazarika@Priyankus-MacBook-Air.local>
2024-05-14 01:13:17 +0530

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="h-full">
+	<header class="h-16 flex justify-start items-center border-b p-4">
+		<h1>X Profile - Header Contrast Checker</h1>
+	</header>
+	<main class="min-h-full p-4">
+		<slot />
+	</main>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<div class="flex justify-center items-center h-full">
+	<div class="flex flex-col items-center">
+		<h1 class="text-6xl font-extrabold">Contrast Checker</h1>
+		<p class="text-xl text-slate-600 mt-4">
+			Calculate the contrast ratio of your twitter banner background and profile picture background
+		</p>
+
+		<div class="mt-14 border rounded-2xl p-8 h-[500px] w-[600px] flex flex-col">
+			<div class="flex justify-between items-center">
+				<div class="flex flex-col w-[45%]">
+					<p>Banner background color</p>
+					<input class="border rounded-md mt-2 h-10" />
+				</div>
+				<div class="flex flex-col w-[45%]">
+					<p>Profile background color</p>
+					<input class="border rounded-md mt-2 h-10" />
+				</div>
+			</div>
+
+			<p class="mt-8">Contrast</p>
+			<div class="flex justify-between items-center mt-4 w-full rounded-lg p-4 bg-green-100">
+				haha
+			</div>
+		</div>
+	</div>
+</div>
